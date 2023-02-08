7c2feffd854e48ef71adcd3bae4d8a877a0e6571
Marlon Wißkirchen <marlon.wisskirchen@outlook.de>
2023-02-08 17:41:15 +0100

    removed bought amount from create shopping list item input

--- lib/domain/dto/create_shopping_list_item_dto.dart
+++ lib/domain/dto/create_shopping_list_item_dto.dart
@@ -2,7 +2,6 @@ class CreateShoppingListItemDto {
   String itemName;
   String? unit;
   double amount;
-  double? boughtAmount;
   String userToBuyItem;
   String privateEvent;
 
@@ -10,7 +9,6 @@ class CreateShoppingListItemDto {
     required this.itemName,
     this.unit,
     required this.amount,
-    this.boughtAmount,
     required this.userToBuyItem,
     required this.privateEvent,
   });
@@ -20,7 +18,6 @@ class CreateShoppingListItemDto {
       'itemName': itemName,
       'unit': unit,
       'amount': amount,
-      'boughtAmount': boughtAmount,
       'userToBuyItem': userToBuyItem,
       'privateEvent': privateEvent,
     };
