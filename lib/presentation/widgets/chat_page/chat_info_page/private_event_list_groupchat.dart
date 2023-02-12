f3acd19a7641133e3b57273cc9af0ec32051b143
Marlon Wißkirchen <marlon.wisskirchen@outlook.de>
2023-02-12 15:55:14 +0100

    chat info page replace private event cubit with current chat cubit for private events

--- lib/presentation/widgets/chat_page/chat_info_page/private_event_list_groupchat.dart
+++ lib/presentation/widgets/chat_page/chat_info_page/private_event_list_groupchat.dart
@@ -1,11 +1,8 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
 import 'package:skeletons/skeletons.dart';
 import 'package:social_media_app_flutter/application/bloc/chat/current_chat_cubit.dart';
-import 'package:social_media_app_flutter/application/bloc/private_event/private_event_cubit.dart';
-import 'package:social_media_app_flutter/domain/entities/private_event/private_event_entity.dart';
 import 'package:social_media_app_flutter/presentation/router/router.gr.dart';
 
 class PrivateEventListGroupchat extends StatelessWidget {
@@ -14,81 +11,70 @@ class PrivateEventListGroupchat extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<PrivateEventCubit, PrivateEventState>(
-      //TODO: save the events in an own store for the current groupchat private events cubit like with the current groupchat in the currentPrivateEventGroupchatStore
-      builder: (context, state) {
-        List<Widget> widgetsToReturn = [];
-        List<PrivateEventEntity> filteredEvents = state.privateEvents
-            .where(
-              (element) =>
-                  element.connectedGroupchat == chatState.currentChat.id,
-            )
-            .toList();
+    List<Widget> widgetsToReturn = [];
 
-        if (filteredEvents.isEmpty && state is PrivateEventLoading) {
-          widgetsToReturn.add(
-            SkeletonListTile(
-              padding: const EdgeInsets.all(8),
-              hasSubtitle: true,
-              titleStyle: const SkeletonLineStyle(width: 100, height: 22),
-              subtitleStyle:
-                  const SkeletonLineStyle(width: double.infinity, height: 16),
-              leadingStyle: const SkeletonAvatarStyle(
-                shape: BoxShape.circle,
+    if (chatState.privateEvents.isEmpty && chatState.loadingPrivateEvents) {
+      widgetsToReturn.add(
+        SkeletonListTile(
+          padding: const EdgeInsets.all(8),
+          hasSubtitle: true,
+          titleStyle: const SkeletonLineStyle(width: 100, height: 22),
+          subtitleStyle:
+              const SkeletonLineStyle(width: double.infinity, height: 16),
+          leadingStyle: const SkeletonAvatarStyle(
+            shape: BoxShape.circle,
+          ),
+        ),
+      );
+    } else {
+      for (final privateEvent in chatState.privateEvents) {
+        widgetsToReturn.add(
+          ListTile(
+            leading: CircleAvatar(
+              backgroundImage: privateEvent.coverImageLink != null
+                  ? NetworkImage(privateEvent.coverImageLink!)
+                  : null,
+              backgroundColor: privateEvent.coverImageLink == null
+                  ? Theme.of(context).colorScheme.secondaryContainer
+                  : null,
+            ),
+            title: Hero(
+              tag: "${privateEvent.id} title",
+              child: Text(
+                privateEvent.title ?? "Kein Titel",
+                style: Theme.of(context).textTheme.titleMedium,
               ),
             ),
-          );
-        } else {
-          for (final event in filteredEvents) {
-            widgetsToReturn.add(
-              ListTile(
-                leading: CircleAvatar(
-                  backgroundImage: event.coverImageLink != null
-                      ? NetworkImage(event.coverImageLink!)
-                      : null,
-                  backgroundColor: event.coverImageLink == null
-                      ? Theme.of(context).colorScheme.secondaryContainer
-                      : null,
-                ),
-                title: Hero(
-                  tag: "${event.id} title",
-                  child: Text(
-                    event.title ?? "Kein Titel",
-                    style: Theme.of(context).textTheme.titleMedium,
-                  ),
-                ),
-                subtitle: Text(
-                  event.eventDate != null
-                      ? DateFormat.yMd().add_jm().format(event.eventDate!)
-                      : "Kein Datum",
-                  softWrap: true,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                onTap: () {
-                  AutoRouter.of(context).push(
-                    PrivateEventWrapperPageRoute(
-                      loadPrivateEventFromApiToo: true,
-                      privateEventToSet: event,
-                      privateEventId: event.id,
-                    ),
-                  );
-                },
-              ),
-            );
-          }
-        }
-
-        return Column(
-          children: [
-            Text(
-              "Private Events: ${widgetsToReturn.length}",
-              style: Theme.of(context).textTheme.titleMedium,
+            subtitle: Text(
+              privateEvent.eventDate != null
+                  ? DateFormat.yMd().add_jm().format(privateEvent.eventDate!)
+                  : "Kein Datum",
+              softWrap: true,
+              overflow: TextOverflow.ellipsis,
             ),
-            if (widgetsToReturn.isNotEmpty) ...{const SizedBox(height: 8)},
-            ...widgetsToReturn
-          ],
+            onTap: () {
+              AutoRouter.of(context).push(
+                PrivateEventWrapperPageRoute(
+                  loadPrivateEventFromApiToo: true,
+                  privateEventToSet: privateEvent,
+                  privateEventId: privateEvent.id,
+                ),
+              );
+            },
+          ),
         );
-      },
+      }
+    }
+
+    return Column(
+      children: [
+        Text(
+          "Private Events: ${widgetsToReturn.length}",
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
+        if (widgetsToReturn.isNotEmpty) ...{const SizedBox(height: 8)},
+        ...widgetsToReturn
+      ],
     );
   }
 }
