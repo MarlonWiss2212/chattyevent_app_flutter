b62261d0da8f935adaf3c39fd392a06b772c2b76
Marlon Wißkirchen <marlon.wisskirchen@outlook.de>
2023-02-14 18:32:19 +0100

    fix profile image loading

--- lib/infastructure/respositories/user_repository_impl.dart
+++ lib/infastructure/respositories/user_repository_impl.dart
@@ -24,6 +24,7 @@ class UserRepositoryImpl implements UserRepository {
             firstname
             lastname
             username
+            profileImageLink
             email
           }
         }
