3d6c7bb08f417aa3d34ae107751882d9f8419f5f
Marlon Wißkirchen <marlon.wisskirchen@outlook.de>
2023-04-02 17:41:14 +0200

    messages not showing fix

--- lib/domain/entities/groupchat/groupchat_entity.dart
+++ lib/domain/entities/groupchat/groupchat_entity.dart
@@ -110,6 +110,7 @@ class GroupchatEntity {
       profileImageLink:
           newEntity.profileImageLink ?? oldEntity.profileImageLink,
       users: users,
+      messages: messages,
       leftUsers: leftUsers,
       description: newEntity.description ?? oldEntity.description,
       createdBy: newEntity.createdBy ?? oldEntity.createdBy,
