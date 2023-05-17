da86f4f11dab6a2209bde5461568b126c6ca7d12
Marlon Wißkirchen <marlon.wisskirchen@outlook.de>
2023-05-17 20:24:27 +0200

    removed userIds from filter

--- lib/core/filter/user/find_users_filter.dart
+++ lib/core/filter/user/find_users_filter.dart
@@ -1,19 +1,12 @@
 class FindUsersFilter {
   final String? search;
-  final List<String>? userIds;
 
-  FindUsersFilter({
-    this.search,
-    this.userIds,
-  });
+  FindUsersFilter({this.search});
 
   Map<dynamic, dynamic> toMap() {
     Map<dynamic, dynamic> map = {};
     if (search != null) {
       map.addAll({"search": search});
-    }
-    if (userIds != null) {
-      map.addAll({"userIds": userIds});
     }
     return map;
   }
